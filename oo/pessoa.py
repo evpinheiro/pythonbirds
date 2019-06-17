e2b61f1ccff64266b72957c31dad7aa23c367fe8
Eder Vasco Pinheiro <eder.pinheiro@wplex.com.br>
2019-06-17 18:30:32 -0300

    criado e removido atributo dinamico de objeto do tipo pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -7,11 +7,31 @@ class Pessoa:
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
+    @staticmethod
+    def static_method():
+        return 42
+
+class Filho(Pessoa):
+    Pessoa.filhos = None
+
+
 if __name__ == '__main__':
     eder = Pessoa(nome='Eder')
-    luciano = Pessoa(eder, nome='Luciano')
+    renzo = Filho(nome='Renzo')
+    print(renzo.cumprimentar())
+    print(eder.cumprimentar())
+    luciano = Pessoa(eder, renzo, nome='Luciano')
     print(Pessoa.cumprimentar(luciano))
     print(id(luciano))
     print(luciano.cumprimentar())
     print(luciano.nome)
     print(luciano.idade)
+    for filho in luciano.filhos:
+        print(filho.nome)
+    eder.sobrenome = "pinheiro"
+    del renzo.filhos
+    print("dict_eder", eder.__dict__)
+    print("dict_renzo", renzo.__dict__)
+
+    print(luciano.static_method())
+    print(Pessoa.static_method())
