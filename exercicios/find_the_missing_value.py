d5691c0484c7f16c6c25286fec7444530431ed12
Eder Vasco Pinheiro <eder.pinheiro@wplex.com.br>
2019-07-02 19:24:54 -0300

    ajustando e comentando módulo

--- exercicios/find_the_missing_value.py
+++ exercicios/find_the_missing_value.py
@@ -1,7 +1,4 @@
-from typing import List
-
-
-def find_missing_value_linear_runtime(listed_numbers:List[int], subset_size: int):
+def find_missing_value_linear_runtime(listed_numbers: list[int], subset_size: int):
     """
     This method aims finding the missing values from the subset
     of natural numbers lower than a number n
@@ -16,12 +13,16 @@ def find_missing_value_linear_runtime(listed_numbers:List[int], subset_size: int
     >>> find_missing_value_linear_runtime(listed_values, 11)
     [6, 10]
     """
+    # Preparing the list to detect missing numbers
     indexed_list = [int]*subset_size
     for number in listed_numbers:
         indexed_list[number-1] = number  # python list indexes starts at 0
+
+    # Detecting the missing numbers
     missing_numbers = []
     for index, indexed_number in enumerate(indexed_list):
         expected_number = index+1
         if indexed_number != expected_number:
-            missing_numbers.append(expected_number)  # python list indexes starts at 0
+            missing_numbers.append(expected_number)
+
     return missing_numbers
